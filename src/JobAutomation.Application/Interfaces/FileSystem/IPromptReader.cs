d606045cef3228d1938898e8ecb560097cf09a57
Sanskar Bosmia <sanskarbosmia@gmail.com>
2026-01-03 11:26:36 +0530

    feat: added ipromptreader interface

---
+++ src/JobAutomation.Application/Interfaces/FileSystem/IPromptReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JobAutomation.Application.Interfaces.FileSystem
+{
+    public interface IPromptReader
+    {
+        Task<string> ReadAsync(string promptName, CancellationToken cancellationToken = default);
+    }
+}
