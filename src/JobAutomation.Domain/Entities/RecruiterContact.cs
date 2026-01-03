86a461d08409cdc1fd57a70a1b0eeb29a7296478
Sanskar Bosmia <sanskarbosmia@gmail.com>
2026-01-03 11:10:56 +0530

    feat: added recruiterContact entity

---
+++ src/JobAutomation.Domain/Entities/RecruiterContact.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobAutomation.Domain.Common;
+
+namespace JobAutomation.Domain.Entities
+{
+    public class RecruiterContact : BaseEntity
+    {
+        public Guid CompanyId { get; private set; }
+
+        public string Name { get; private set; }
+
+        public string Email { get; private set; }
+
+        public bool AddedManually { get; private set; }
+
+        private RecruiterContact() { }
+
+        public RecruiterContact(
+            Guid companyId,
+            string email,
+            string name = null,
+            bool addedManually = true)
+        {
+            CompanyId = companyId;
+            Email = email;
+            Name = name;
+            AddedManually = addedManually;
+        }
+
+        public void UpdateName(string name)
+        {
+            Name = name;
+        }
+
+        public void UpdateEmail(string email)
+        {
+            Email = email;
+        }
+    }
+}
