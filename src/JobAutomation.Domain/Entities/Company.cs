79cf75bf62d2eb97b439a96c044b208799b13f41
Sanskar Bosmia <sanskarbosmia@gmail.com>
2026-01-03 11:08:56 +0530

    feat: added company entity

---
+++ src/JobAutomation.Domain/Entities/Company.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobAutomation.Domain.Common;
+
+namespace JobAutomation.Domain.Entities
+{
+    public class Company : BaseEntity
+    {
+        public string Name { get; private set; }
+        public string CareerPageUrl { get; private set; }
+        public string Website { get; private set; }
+
+        private Company() { }
+
+        public Company(string name, string careerPageUrl, string website)
+        {
+            Name = name;
+            CareerPageUrl = careerPageUrl;
+            Website = website;
+        }
+
+        public void UpdateCareerPage(string careerPageUrl)
+        {
+            CareerPageUrl = careerPageUrl;
+        }
+
+        public void UpdateWebsite(string website)
+        {
+            Website = website;
+        }
+    }
+}
