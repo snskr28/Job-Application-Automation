0d538c2018ffb97206032555cdc1cb2d822b3565
Sanskar Bosmia <sanskarbosmia@gmail.com>
2026-01-03 11:12:02 +0530

    feat: added emailDraft entity

---
+++ src/JobAutomation.Domain/Entities/EmailDraft.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JobAutomation.Domain.Common;
+
+namespace JobAutomation.Domain.Entities
+{
+    public class EmailDraft : BaseEntity
+    {
+        public Guid JobId { get; private set; }
+        public string Subject { get; private set; }
+        public string Body { get; private set; }
+        public bool GeneratedByAI { get; private set; }
+
+        private EmailDraft() { }
+
+        public EmailDraft(
+            Guid jobId,
+            string subject,
+            string body,
+            bool generatedByAI)
+        {
+            JobId = jobId;
+            Subject = subject;
+            Body = body;
+            GeneratedByAI = generatedByAI;
+        }
+
+        public void UpdateContent(string subject, string body)
+        {
+            Subject = subject;
+            Body = body;
+            GeneratedByAI = false;
+        }
+    }
+}
