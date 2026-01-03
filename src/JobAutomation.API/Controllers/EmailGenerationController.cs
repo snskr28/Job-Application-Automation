681058b63bc90e5c6b409b52fa3ef13881cba577
Sanskar Bosmia <sanskarbosmia@gmail.com>
2026-01-03 13:22:38 +0530

    feat: registered GenerateEmailHandler to DI

---
+++ src/JobAutomation.API/Controllers/EmailGenerationController.cs
@@ -0,0 +1,20 @@
+﻿using JobAutomation.Application.Features.EmailGeneration;
+using JobAutomation.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobAutomation.API.Controllers
+{
+    [ApiController]
+    [Route("api/email")]
+    public class EmailGenerationController : ControllerBase
+    {
+        private readonly GenerateEmailHandler _handler;
+
+        public EmailGenerationController(GenerateEmailHandler handler)
+        {
+            _handler = handler;
+        }
+        
+    }
+}
--- src/JobAutomation.API/Program.cs
+++ src/JobAutomation.API/Program.cs
@@ -1,3 +1,4 @@
+using JobAutomation.Application.Features.EmailGeneration;
 using JobAutomation.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddScoped<GenerateEmailHandler>();
+
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -14,6 +17,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
